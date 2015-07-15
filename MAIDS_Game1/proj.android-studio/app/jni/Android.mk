2124c0aaadc1d557d43605ef70696a597a0756a1
HW-Lee <tony123930@gmail.com>
2015-07-15 18:10:05 +0800

    modified Android.mk in Android Studio Project

--- MAIDS_Game1/proj.android-studio/app/jni/Android.mk
+++ MAIDS_Game1/proj.android-studio/app/jni/Android.mk
@@ -14,7 +14,11 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../../Classes/AppDelegate.cpp \
                    ../../../Classes/HelloWorldScene.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+					$(LOCAL_PATH)/../../../Classes/Unit \
+					$(LOCAL_PATH)/../../../Classes/Interface \
+					$(LOCAL_PATH)/../../../Classes/Callback \
+					$(LOCAL_PATH)/../../../Classes/Utils
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
